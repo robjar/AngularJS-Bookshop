1ada63559588f2a5a37218fefee6640b561b2b5d
Robert <rob_jar@wp.pl>
2016-03-05 22:34:47 +0100

    Add bookListWrapper and BookListCtrl

---
+++ app/js/bookListCtrl.js
@@ -0,0 +1,11 @@
+export default function BookListCtrl() {
+  var vm = this;
+  vm.books = [{
+    name: 'Adultery',
+    price: 200,
+    rating: 4,
+    publisher: 'You&me',
+    releaseDate: '30-02-2012',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quae doloremque dolores minus alias doloribus optio dolore, perspiciatis ullam. Nobis.'
+  }];
+}
--- app/js/headerCtrl.js
+++ app/js/headerCtrl.js
@@ -1,5 +1,5 @@
 export default function HeaderCtrl() {
   var vm = this;
-  vm.title = "Bookshop";
-  vm.tagline = "The greatest collection of books in the world!";
+  vm.title = 'Bookshop';
+  vm.tagline = 'The greatest collection of books in the world!';
 }
